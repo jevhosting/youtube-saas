5df1a19e97be2a5d912216df6e3db7842abddf78
Jose Vargas <jevhosting@gmail.com>
2026-04-23 19:45:15 -0400

    connect to VPS backend

--- features/transcription/processVideo.js
+++ features/transcription/processVideo.js
@@ -1,5 +1,5 @@
 export async function processYouTubeVideo(url) {
-  const res = await fetch("https://lemura-backend-production.up.railway.app/transcribe", {
+  const res = await fetch("http://157.230.60.238:3000/transcribe", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
